e2a4d790ce3e7d278310646343575c238ff7cadb
Shadient <85789237+Shadient@users.noreply.github.com>
2023-03-19 11:23:46 +0100

    Update ricing.sh

--- ricing.sh
+++ ricing.sh
@@ -3,8 +3,8 @@ sudo apt install i3 polybar rofi -y
 
 ## i3
 mkdir -p ~/.config/i3
-curl https://raw.githubusercontent.com/Shadient/linux-mint-config/i3/config > ~/.config/i3/config
+curl https://raw.githubusercontent.com/Shadient/linux-mint-config/main/i3/config > ~/.config/i3/config
 
 ## Polybar
 mkdir -p ~/.config/polybar
-curl https://raw.githubusercontent.com/Shadient/linux-mint-config/polybar/config > ~/.config/polybar/config
+curl https://raw.githubusercontent.com/Shadient/linux-mint-config/main/polybar/config > ~/.config/polybar/config
