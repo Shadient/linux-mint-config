16239f4df69d60e81b07802deb3fbed3fbcd9f19
Shadient <85789237+Shadient@users.noreply.github.com>
2023-03-19 11:12:36 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -30,4 +30,11 @@ sudo apt install i3 polybar rofi -y
 
 ## i3
 mkdir -p ~/.config/i3
-touch ~/.config/i3/config
+curl https://raw.githubusercontent.com/Shadient/linux-mint-config/i3/config > ~/.config/i3/config
+
+## Polybar
+mkdir -p ~/.config/polybar
+curl https://raw.githubusercontent.com/Shadient/linux-mint-config/polybar/config > ~/.config/polybar/config
+
+
+
