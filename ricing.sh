1b7ef910c4f13036d75119dde83f46e988878fad
Shadient <85789237+Shadient@users.noreply.github.com>
2023-03-19 12:17:33 +0100

    Update ricing.sh

--- ricing.sh
+++ ricing.sh
@@ -8,3 +8,6 @@ curl https://raw.githubusercontent.com/Shadient/linux-mint-config/main/i3/config
 ## Polybar
 mkdir -p ~/.config/polybar
 curl https://raw.githubusercontent.com/Shadient/linux-mint-config/main/polybar/config > ~/.config/polybar/config
+curl https://raw.githubusercontent.com/Shadient/linux-mint-config/main/polybar/launch.sh > ~/.config/polybar/launch.sh
+cd .config/polybar
+chmod +x launch.sh
