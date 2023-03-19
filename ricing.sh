ff231844d84f518d4f4917eb4bd4759783f336eb
Shadient <85789237+Shadient@users.noreply.github.com>
2023-03-19 11:15:50 +0100

    Create ricing.sh

---
+++ ricing.sh
@@ -0,0 +1,10 @@
+## RICING
+sudo apt install i3 polybar rofi -y
+
+## i3
+mkdir -p ~/.config/i3
+curl https://raw.githubusercontent.com/Shadient/linux-mint-config/i3/config > ~/.config/i3/config
+
+## Polybar
+mkdir -p ~/.config/polybar
+curl https://raw.githubusercontent.com/Shadient/linux-mint-config/polybar/config > ~/.config/polybar/config
